8bb57b1cc4ae053b799e1a06cca446c513115331
Yuuki <52652158+rene-roid@users.noreply.github.com>
2022-12-20 20:33:57 +0100

    Project Setup
    Setting up folders
    Scene template
    Some scripts and prefabs

---
+++ Assets/_Scripts/Utilities/Helpers.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// A static class for general helpful methods
+/// </summary>
+public static class Helpers 
+{
+    /// <summary>
+    /// Return the camera
+    /// </summary>
+    private static Camera _camera;
+    public static Camera Camera
+    {
+        get
+        {
+            if (_camera == null) _camera = Camera.main;
+            return _camera;
+        }
+    }
+
+
+    /// <summary>
+    /// Cache WaitForSeconds
+    /// </summary>
+    private static readonly Dictionary<float, WaitForSeconds> WaitDictionary = new Dictionary<float, WaitForSeconds>();
+    public static WaitForSeconds GetWait(float time)
+    {
+        if (WaitDictionary.TryGetValue(time, out var wait)) return wait;
+
+        WaitDictionary[time] = new WaitForSeconds(time);
+        return WaitDictionary[time];
+    }
+
+
+    /// <summary>
+    /// Tells you if the pointer is over any UI element
+    /// </summary>
+    private static PointerEventData _eventDataCurrentPosition;
+    private static List<RaycastResult> _results;
+    public static bool IsOverUi()
+    {
+        _eventDataCurrentPosition = new PointerEventData(EventSystem.current) { position = Input.mousePosition };
+        _results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(_eventDataCurrentPosition, _results);
+        return _results.Count > 0;
+    }
+
+
+    /// <summary>
+    /// Returns canvas item in worldspace
+    /// </summary>
+    /// <param name="element">Target element in canvas</param>
+    /// <returns></returns>
+    public static Vector2 GetWorldPositionOfCanvasElement(RectTransform element)
+    {
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(element, element.position, Camera, out var result);
+        return result;
+    }
+
+
+    /// <summary>
+    /// Destroy all child objects of this transform (Unintentionally evil sounding).
+    /// Use it like so:
+    /// <code>
+    /// transform.DestroyChildren();
+    /// </code>
+    /// </summary>
+    public static void DestroyChildren(this Transform t) {
+        foreach (Transform child in t) Object.Destroy(child.gameObject);
+    }
+}
