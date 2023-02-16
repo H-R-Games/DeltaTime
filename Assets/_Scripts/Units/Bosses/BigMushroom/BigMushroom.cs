b7aec95ab7dd7dc894031eb224ee4f6b6dde25d5
Alejandro Font <fontalejandro0@gmail.com>
2023-02-16 18:55:28 +0100

    Create BigMushroom

--- Assets/_Scripts/Units/Bosses/BigMushroom/BigMushroom.cs
+++ Assets/_Scripts/Units/Bosses/BigMushroom/BigMushroom.cs
@@ -11,6 +11,7 @@ namespace rene_roid_enemy
         {
             base.Start();
             ChangeState(EnemyStates.Target);
+            _anim = GetComponentInChildren<Animator>();
         }
 
         public override void Update() { UpdateState(); }
@@ -39,6 +40,8 @@ namespace rene_roid_enemy
                     if (Vector3.Distance(transform.position, _targetPlayer.transform.position) <= _attackRangeDistance) ChangeState(EnemyStates.Attack);
                     break;
             }
+
+            HandleAnimations();
         }
 
         public override void ChangeState(EnemyStates newState)
@@ -85,6 +88,9 @@ namespace rene_roid_enemy
 
             if (!_isGround && !_walled) transform.Translate(directionX * _movementSpeed * _movementSpeedMultiplier * Time.deltaTime);
 
+            if (_movementDirection.x > 0 && Vector3.Distance(new Vector3(_targetPlayer.transform.position.x, 0, 0), new Vector3(this.transform.position.x, 0, 0)) > 0.5f) this.gameObject.GetComponentInChildren<SpriteRenderer>().flipX = false;
+            else if (_movementDirection.x < 0 && Vector3.Distance(new Vector3(_targetPlayer.transform.position.x, 0, 0), new Vector3(this.transform.position.x, 0, 0)) > 0.5f) this.gameObject.GetComponentInChildren<SpriteRenderer>().flipX = true;
+
             _movementDirection = directionX;
         }
         #endregion
@@ -94,6 +100,8 @@ namespace rene_roid_enemy
         [SerializeField] private GameObject _sporesPrefab;
         float _timeAwait = 0;
         float _sporesTimer = 0;
+        bool _attackNibble, _attackSpores, _attackClaps;
+        float _timeAttackSporesAwait = 0;
 
         private void AttackController()
         {
@@ -127,18 +135,72 @@ namespace rene_roid_enemy
 
             if (!watchin) return;
             if (Vector3.Distance(transform.position, _targetPlayer.transform.position) <= _attackRangeDistance / 2 && watchin) _targetPlayer.GetComponent<PlayerBase>().TakeDamage(_damage);
+            _attackNibble = true;
         }
 
         private void AttackSpores()
         {
             _sporesTimer = 8;
             if (Vector3.Distance(transform.position, _targetPlayer.transform.position) <= _attackRangeDistance) Instantiate(_sporesPrefab, transform.position, Quaternion.identity);
+            _attackSpores = true;
         }
 
         private void AttackClaps()
         {
             var p = (_targetPlayer.transform.position - this.transform.position).normalized;
             if (Vector3.Distance(transform.position, _targetPlayer.transform.position) <= _attackRangeDistance / 1.5f) _targetPlayer.GetComponent<PlayerBase>().TakeDamage(_damage);
+            _attackClaps = true;
+        }
+        #endregion
+
+        #region Animation
+        private Animator _anim;
+        private int _currentAnimation = 0;
+        private float _lockedTill;
+        private static readonly int Idle = Animator.StringToHash("Idle");
+        private static readonly int Run = Animator.StringToHash("Run");
+        private static readonly int Death = Animator.StringToHash("Death");
+        private static readonly int StunnedAnim = Animator.StringToHash("Stunned");
+        private static readonly int AttackNibbleAnim = Animator.StringToHash("AttackNibble");
+        private static readonly int AttackSporesAnim = Animator.StringToHash("AttackSpores");
+        private static readonly int AttackClapsAnim = Animator.StringToHash("AttackClaps");
+
+        [SerializeField] private float _attackNibbleAnimTime = 0.5f;
+        [SerializeField] private float _AttackSporesAnimTime = 0.5f;
+        [SerializeField] private float _AttackClapsAnimTime = 0.5f;
+
+        private void HandleAnimations()
+        {
+            var state = GetState();
+            ResetFlags();
+            if (state == _currentAnimation) return;
+
+            _anim.Play(state, 0); //_anim.CrossFade(state, 0, 0);
+            _currentAnimation = state;
+
+            int GetState()
+            {
+                if (Time.time < _lockedTill) return _currentAnimation;
+
+                // ANY SKILL PRESSED
+                // if (_isStunned) return LockState(UltimateAttackAnim, 5);
+                if (_attackNibble) return LockState(AttackNibbleAnim, _attackNibbleAnimTime);
+                if (_attackSpores) return LockState(AttackSporesAnim, _AttackSporesAnimTime);
+                if (_attackClaps) return LockState(AttackClapsAnim, _AttackClapsAnimTime);
+
+                // NO SKILL PRESSED
+                return Run;
+
+                // State and time to lock
+                int LockState(int s, float t)
+                {
+                    _lockedTill = Time.time + t;
+                    return s;
+                }
+
+            }
+
+            void ResetFlags() { _attackNibble = _attackSpores = _attackClaps = false; }
         }
         #endregion
 
