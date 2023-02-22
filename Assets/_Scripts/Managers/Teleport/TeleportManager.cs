68eef6461eb45dbbd14589eb52d6b3aaab2c556a
Alejandro Font <fontalejandro0@gmail.com>
2023-02-22 17:29:37 +0100

    Init

---
+++ Assets/_Scripts/Managers/Teleport/TeleportManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace hrTeleportManager
+{
+    public class TeleportManager : MonoBehaviour
+    {
+        
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
