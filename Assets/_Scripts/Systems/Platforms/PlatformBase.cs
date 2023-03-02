79f8981e5669d6f498f2f35ea33fdec4b861a062
Yuuki <52652158+rene-roid@users.noreply.github.com>
2023-03-02 22:51:16 +0100

    Patrol platform

--- Assets/_Scripts/Systems/Platforms/PlatformBase.cs
+++ Assets/_Scripts/Systems/Platforms/PlatformBase.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using rene_roid_player;
 
-namespace rene_roid {    
+namespace rene_roid
+{
     [RequireComponent(typeof(Rigidbody2D))]
     public abstract class PlatformBase : MonoBehaviour
     {
+        [Tooltip("If velocity is above this threshold the platform will not affect the player")]
+        [SerializeField] private float _unlockThreshold = 2.5f;
         private Rigidbody2D _player;
         protected Rigidbody2D _rb;
         protected Vector2 _startPos;
@@ -16,19 +19,37 @@ namespace rene_roid {
             _startPos = _rb.position;
         }
 
-        protected private void FixedUpdate() {
+        protected virtual void FixedUpdate()
+        {
             var newPos = _rb.position;
             var change = newPos - _lastPos;
             _lastPos = newPos;
 
-            //MovePlayer(change);
+            MovePlayer(change);
         }
 
-        protected virtual void OnCollisionEnter2D(Collision2D other) {
-            if (other.transform.TryGetComponent<PlayerBase>(out var player)) {
+        protected virtual void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.transform.TryGetComponent<PlayerBase>(out var player))
+            {
                 _player = player.GetComponent<Rigidbody2D>();
-                // MovePlayer(Vector2.zero);
+                MovePlayer(Vector2.zero);
             }
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if (other.transform.TryGetComponent<PlayerBase>(out var player))
+            {
+                _player = null;
+            }
+        }
+
+        protected virtual void MovePlayer(Vector2 change)
+        {
+            if (!_player || _player.velocity.magnitude >= _unlockThreshold) return;
+
+            _player.MovePosition(_player.position + change);
         }
     }
 }
