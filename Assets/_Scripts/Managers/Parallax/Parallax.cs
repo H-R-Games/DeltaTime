28eb9e734a8f91206c4136360513c67d3a2f8774
Yuuki <52652158+rene-roid@users.noreply.github.com>
2023-03-10 21:40:48 +0100

    parallax background

--- Assets/_Scripts/Managers/Parallax/Parallax.cs
+++ Assets/_Scripts/Managers/Parallax/Parallax.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
         private Camera _camera;
-        [SerializeField] private float _parallaxFactor = 0.5f;
+        private float _length, _startPos;
+        [SerializeField] private float _parallaxFactor;
+        public bool Child = false;
 
         void Start()
         {
@@ -17,19 +19,22 @@ public class Parallax : MonoBehaviour
             float spriteHeight = spriteRenderer.sprite.bounds.size.y;
             float cameraHeight = _camera.orthographicSize * 2;
             float scale = cameraHeight / spriteHeight;
-            transform.localScale = new Vector3(scale, scale, 1);
+
+            if (!Child) transform.localScale = new Vector3(scale, scale, 1);
+
+            _startPos = transform.position.x;
+            _length = GetComponent<SpriteRenderer>().bounds.size.x;
         }
 
         void Update()
         {
+            float temp = (_camera.transform.position.x * (1 - _parallaxFactor));
+            float dist = (_camera.transform.position.x * _parallaxFactor);
 
-        }
+            transform.position = new Vector3(_startPos + dist, _camera.transform.position.y, transform.position.z);
 
-        void LateUpdate()
-        {
-            Vector3 cameraPosition = _camera.transform.position;
-            Vector3 parallaxPosition = new Vector3(cameraPosition.x * _parallaxFactor, cameraPosition.y, transform.position.z);
-            transform.position = parallaxPosition;
+            if (temp > _startPos + _length) _startPos += _length;
+            else if (temp < _startPos - _length) _startPos -= _length;
         }
     
 }
