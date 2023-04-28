ca6fc0b13d057637a9f0ace64115582d4969d6f7
Yuuki <52652158+rene-roid@users.noreply.github.com>
2023-04-28 07:42:38 +0200

    Mastron setup
    (player cant move idk why)

---
+++ Assets/_Scripts/Units/Player/Mastron/Mastron.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace rene_roid_player {    
+    public class Mastron : PlayerBase
+    {
+        void Start()
+        {
+            
+        }
+        
+        void Update()
+        {
+            
+        }
+    }
+}
