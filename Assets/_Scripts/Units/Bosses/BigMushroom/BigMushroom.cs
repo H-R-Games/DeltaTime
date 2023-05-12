e05fe7c96c644b8ae2732ec0447e232a9d39ff43
Alejandro Font <fontalejandro0@gmail.com>
2023-05-12 16:54:02 +0200

    BOSS

--- Assets/_Scripts/Units/Bosses/BigMushroom/BigMushroom.cs
+++ Assets/_Scripts/Units/Bosses/BigMushroom/BigMushroom.cs
@@ -13,8 +13,10 @@ namespace rene_roid_enemy
             base.Start();
             ChangeState(EnemyStates.Target);
             _anim = GetComponentInChildren<Animator>();
-            _colliderNibble.GetComponent<Collider2D>().enabled = false;
-            _colliderClap.GetComponent<Collider2D>().enabled = false;
+            _colliderNibble = transform.GetChild(1).GetComponent<BoxCollider2D>();
+            _colliderClap = transform.GetChild(2).GetComponent<BoxCollider2D>();
+            _colliderNibble.GetComponent<BoxCollider2D>().enabled = false;
+            _colliderClap.GetComponent<BoxCollider2D>().enabled = false;
         }
 
         public override void Update() { UpdateState(); }
@@ -93,12 +95,10 @@ namespace rene_roid_enemy
             if (_movementDirection.x > 0 && Vector3.Distance(new Vector3(_targetPlayer.transform.position.x, 0, 0), new Vector3(this.transform.position.x, 0, 0)) > 0.5f)
             {
                 this.gameObject.GetComponentInChildren<SpriteRenderer>().flipX = _movementDirection.x < 0;
-                _colliderNibble.transform.localScale = new Vector3(1, 1, 1);
             } 
             else if (_movementDirection.x < 0 && Vector3.Distance(new Vector3(_targetPlayer.transform.position.x, 0, 0), new Vector3(this.transform.position.x, 0, 0)) > 0.5f)
             {
                 this.gameObject.GetComponentInChildren<SpriteRenderer>().flipX = _movementDirection.x < 0;
-                _colliderNibble.transform.localScale = new Vector3(-1, 1, 1);
             } 
 
             _movementDirection = directionX;
@@ -109,8 +109,8 @@ namespace rene_roid_enemy
         [Header("Attack Settings")]
         [SerializeField] private GameObject _sporesPrefabs;
         [SerializeField] private float _sporesCooldown = 1f;
-        [SerializeField] private GameObject _colliderNibble;
-        [SerializeField] private GameObject _colliderClap;
+        [SerializeField] private BoxCollider2D _colliderNibble;
+        [SerializeField] private BoxCollider2D _colliderClap;
         [SerializeField] private int _sporesCount = 3;
         bool _attackSpores, _attackClap, _attackNibble;
         float _timerSpores;
@@ -130,33 +130,42 @@ namespace rene_roid_enemy
                     break;
                 case 1:
                     StartCoroutine(ClapAttack());
+                    SetAttack();
                     break;
                 case 2:
                     StartCoroutine(NibbleAttack());
+                    SetAttack();
                     break;
             }
         }
 
         private IEnumerator SporesAttack()
         {
+            if (_attackSpores) yield break;
             _attackSpores = true;
+            _attackSporesAnim = true;
             ChangeState(EnemyStates.Idle);
             Instantiate(_sporesPrefabs, this.transform.position, Quaternion.identity);
             yield return Helpers.GetWait(_attackSpoeresAnimTime);
+            _attackSpores = false;
             ChangeState(EnemyStates.Target);
         }
 
         private IEnumerator ClapAttack()
         {
+            if (_attackClap) yield break;
             _attackClap = true;
+            _attackClapAnim = true;
             ChangeState(EnemyStates.Idle);
 
-            _colliderClap.transform.position = transform.position;
-            _colliderClap.transform.Translate(new Vector3(_movementDirection.x > 0 ? 1.5f : -1.5f, 0, 0));
+            _colliderClap.GetComponent<Transform>().transform.position = transform.position;
+            _colliderClap.GetComponent<Transform>().transform.Translate(new Vector3(_movementDirection.x > 0 ? 1.5f : -1.5f, 0, 0));
 
-            var players = Physics2D.OverlapBoxAll(_boxCollider2D.bounds.center, _boxCollider2D.bounds.size, 0, _playerLayer);
+            yield return Helpers.GetWait(0.5f);
 
-            _colliderClap.GetComponent<Collider2D>().enabled = true;
+            _colliderClap.enabled = true;
+
+            var players = Physics2D.OverlapBoxAll(_colliderClap.bounds.center, _colliderClap.bounds.size, 0, _playerLayer);
             
             foreach (var player in players)
             {
@@ -166,30 +175,37 @@ namespace rene_roid_enemy
             
             yield return Helpers.GetWait(_attackClapAnimTime + 0.5f);
             
-            _colliderClap.GetComponent<Collider2D>().enabled = false;
+            _colliderClap.enabled = false;
+            _attackClap = false;
             ChangeState(EnemyStates.Target);
         }
 
         private IEnumerator NibbleAttack()
         {
+            if (_attackNibble) yield break;
             _attackNibble = true;
+            _attackNibbleAnim = true;
             ChangeState(EnemyStates.Idle);
 
-            _colliderNibble.transform.position = transform.position;
-            _colliderNibble.transform.Translate(new Vector3(_movementDirection.x > 0 ? 1.5f : -1.5f, 0, 0));
+            _colliderNibble.GetComponent<Transform>().transform.position = transform.position;
+            _colliderNibble.GetComponent<Transform>().transform.Translate(new Vector3(_movementDirection.x > 0 ? 1.5f : -1.5f, 0, 0));
 
-            var players = Physics2D.OverlapBoxAll(_boxCollider2D.bounds.center, _boxCollider2D.bounds.size, 0, _playerLayer);
+            yield return Helpers.GetWait(0.5f);
+
+            _colliderNibble.enabled = true;
+
+            var players = Physics2D.OverlapBoxAll(_colliderNibble.bounds.center, _colliderNibble.bounds.size, 0, _playerLayer);
 
             foreach (var player in players)
             {
-                _colliderNibble.GetComponent<Collider2D>().enabled = true;
                 var playerBase = player.GetComponent<PlayerBase>();
                 if (playerBase != null) playerBase.TakeDamage(_damage);
             }
 
             yield return Helpers.GetWait(_attackNibbleAnimTime + 0.5f);
             
-            _colliderNibble.GetComponent<Collider2D>().enabled = false;
+            _colliderNibble.enabled = false;
+            _attackNibble = false;
             ChangeState(EnemyStates.Target);
         }
 
@@ -207,6 +223,9 @@ namespace rene_roid_enemy
             private static readonly int AttackClaps = Animator.StringToHash("AttackClaps");
             private static readonly int AttackNibble = Animator.StringToHash("AttackNibble");
             bool _idleAnim;
+            bool _attackSporesAnim;
+            bool _attackClapAnim;
+            bool _attackNibbleAnim;
 
             [Header("Animation Settings")]
             [SerializeField] private float _attackSpoeresAnimTime = 0.5f;
@@ -227,9 +246,9 @@ namespace rene_roid_enemy
                     if (Time.time < _lockedTill) return _currentAnimation;
 
                     // ANY SKILL PRESSED
-                    if (_attackSpores) return LockState(AttackSpores, _attackSpoeresAnimTime);
-                    if (_attackClap) return LockState(AttackClaps, _attackClapAnimTime);
-                    if (_attackNibble) return LockState(AttackNibble, _attackNibbleAnimTime);
+                    if (_attackSporesAnim) return LockState(AttackSpores, _attackSpoeresAnimTime);
+                    if (_attackClapAnim) return LockState(AttackClaps, _attackClapAnimTime);
+                    if (_attackNibbleAnim) return LockState(AttackNibble, _attackNibbleAnimTime);
                     if (_isStunned) return LockState(StunnedAnim, 5);
                     if (_idleAnim) return Idle;
 
@@ -245,7 +264,7 @@ namespace rene_roid_enemy
 
                 }
 
-                void ResetFlags() { _attackSpores = _attackClap = _attackNibble = false;}
+                void ResetFlags() { _attackSporesAnim = _attackClapAnim = _attackNibbleAnim = false;}
             }
         #endregion
     }
